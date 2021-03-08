68c434bb186a53afa36bbb1853d1ed08cf6bd08c
etychangucdavis <64785646+etychangucdavis@users.noreply.github.com>
2021-03-09 00:06:44 +0800

    Update gacha.ts

--- src/commands/gacha.ts
+++ src/commands/gacha.ts
@@ -13,18 +13,17 @@ export function setupGacha(bot: Telegraf<ContextMessageUpdate>) {
     // Get chat
     const chat = await findChat(ctx.chat.id)
 
-    var gachas = [0, 0, 0, 10, 10, 10, 20, 20, 20, 30, 30, 30, 50, 50, 50, 100, 100, 200, 200, 500];
-    var idx = Math.floor(Math.random() * gachas.length);
-    var text = '';
+    const gachas = [0, 0, 0, 10, 10, 10, 20, 20, 20, 30, 30, 30, 50, 50, 50, 100, 100, 200, 200, 500];
+    const idx = Math.floor(Math.random() * gachas.length);
 
-    if (gachas[idx] == 0) text = '0';
-    if (gachas[idx] == 10) text = '10';
-    if (gachas[idx] == 20) text = '20';
-    if (gachas[idx] == 30) text = '30';
-    if (gachas[idx] == 50) text = '50';
-    if (gachas[idx] == 100) text = '100';
-    if (gachas[idx] == 200) text = '200';
-    if (gachas[idx] == 500) text = '500';
+    if (gachas[idx] == 0) const text = '0';
+    if (gachas[idx] == 10) const text = '10';
+    if (gachas[idx] == 20) const text = '20';
+    if (gachas[idx] == 30) const text = '30';
+    if (gachas[idx] == 50) const text = '50';
+    if (gachas[idx] == 100) const text = '100';
+    if (gachas[idx] == 200) const text = '200';
+    if (gachas[idx] == 500) const text = '500';
 
     // Reply
     ctx.reply(loc(text, chat.language), {
